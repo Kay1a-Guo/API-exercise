3455efc69036a27be7f8696d0381a840aee5c32d
Kayla Guo <guoka3@oocl.com>
2025-08-31 16:20:20 +0800

    feature:finish todo controller

--- src/main/java/com/oocl/training/controller/dto/TodoRequest.java
+++ src/main/java/com/oocl/training/controller/dto/TodoRequest.java
@@ -1,4 +1,22 @@
 package com.oocl.training.controller.dto;
 
 public class TodoRequest {
+
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public TodoRequest(String title) {
+        this.title = title;
+    }
+
+    public TodoRequest() {
+    }
+
 }
--- src/main/java/com/oocl/training/controller/mapper/TodoMapper.java
+++ src/main/java/com/oocl/training/controller/mapper/TodoMapper.java
@@ -1,4 +1,24 @@
 package com.oocl.training.controller.mapper;
 
+import com.oocl.training.controller.dto.TodoRequest;
+import com.oocl.training.model.Todo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+@Component
 public class TodoMapper {
+    public Todo toResponse(Todo todo){
+        Todo response = new Todo();
+        BeanUtils.copyProperties(todo, response);
+        return response;
+    }
+    public List<Todo> toResponseList(List<Todo> todos){
+        return todos.stream().map(this::toResponse).toList();
+    }
+    public Todo toEntity(TodoRequest todo){
+        Todo response = new Todo();
+        BeanUtils.copyProperties(todo, response);
+        return response;
+    }
 }
